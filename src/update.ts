d60819c9bd7d966030ac14f032077f9157728c11
meisZWFLZ <atcurtis08@gmail.com>
2023-12-01 08:01:51 -0700

    fix: fix update.ts exports

--- src/update.ts
+++ src/update.ts
@@ -4,51 +4,54 @@ import { renameSync, unlinkSync, createWriteStream } from "fs";
 import child_process = require("child_process");
 import { get as httpGet, type IncomingMessage } from "http";
 import { standalone } from "../config.json";
-if (!standalone)
-  /**
-   * self updates the cli executable
-   * @warning DO NOT USE THIS IF NOT PART OF STANDALONE EXECUTABLE
-   */
-  module.exports = async function updateCLI(
-    downloadUrl: string,
-  ): Promise<void> {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const filePath = process.env._!;
-    const newPath = filePath + "-new";
-    const oldPath = filePath + "-old";
-    try {
-      console.log("attempting to delete old file");
-      unlinkSync(oldPath);
-    } catch (err) {
-      console.log("Downloading new file...");
-      const fileStream = createWriteStream(newPath);
 
-      const response = await new Promise<IncomingMessage>((resolve) =>
-        httpGet(downloadUrl, resolve),
-      );
+/**
+ * self updates the cli executable
+ * @warning DO NOT USE THIS IF NOT PART OF STANDALONE EXECUTABLE
+ */
+export async function updateCLI(downloadUrl: string): Promise<void> {
+  if (!standalone)
+    throw new Error(
+      "LemLink is not part of a standalone executable, and thus cannot self update",
+    );
 
-      response.pipe(fileStream);
-      await new Promise((resolve) => fileStream.on("finish", resolve));
-      fileStream.close();
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const filePath = process.env._!;
+  const newPath = filePath + "-new";
+  const oldPath = filePath + "-old";
+  try {
+    console.log("attempting to delete old file");
+    unlinkSync(oldPath);
+  } catch (err) {
+    console.log("Downloading new file...");
+    const fileStream = createWriteStream(newPath);
 
-      console.log("File downloaded successfully.");
+    const response = await new Promise<IncomingMessage>((resolve) =>
+      httpGet(downloadUrl, resolve),
+    );
 
-      console.log("Replacing current file...");
-      renameSync(filePath, oldPath);
-      renameSync(newPath, filePath);
-      console.log("Replaced current file successfully!");
+    response.pipe(fileStream);
+    await new Promise((resolve) => fileStream.on("finish", resolve));
+    fileStream.close();
 
-      console.log("Restarting...");
-      child_process.spawn(
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        process.argv.shift()!,
-        process.argv,
-        {
-          cwd: process.cwd(),
-          detached: true,
-          stdio: "inherit",
-          shell: true,
-        },
-      );
-    }
-  };
+    console.log("File downloaded successfully.");
+
+    console.log("Replacing current file...");
+    renameSync(filePath, oldPath);
+    renameSync(newPath, filePath);
+    console.log("Replaced current file successfully!");
+
+    console.log("Restarting...");
+    child_process.spawn(
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      process.argv.shift()!,
+      process.argv,
+      {
+        cwd: process.cwd(),
+        detached: true,
+        stdio: "inherit",
+        shell: true,
+      },
+    );
+  }
+}
