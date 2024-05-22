a6f7dc1528d093225b09ce423cfdb1539fe9764d
meisZWFLZ <atcurtis08@gmail.com>
2024-05-21 21:48:01 -0700

    feat: :sparkles: write some more example cli code
    get some experience with rust / clap

--- packages/lemlink/src/main.rs
+++ packages/lemlink/src/main.rs
@@ -1,22 +1,62 @@
 use clap::Parser as _;
 
 #[derive(clap::Parser)]
-struct Args {
+
+struct TopLevelArgs {
     #[clap(subcommand)]
     command: Command,
 }
 
 #[derive(clap::Subcommand)]
+enum ConductorSubcommand {
+    CreateTemplate(CreateTemplateArgs),
+}
+
+#[derive(clap::Parser, Debug)]
+#[command(hide = true)]
+struct CreateTemplateArgs {
+    #[arg()]
+    path: String,
+    #[arg()]
+    name: String,
+    #[arg()]
+    version: String,
+    #[clap(long)]
+    system: Vec<String>,
+    #[clap(long)]
+    user: Vec<String>,
+    #[clap(long, value_enum, value_name = "TARGET", num_args = 1 )]
+    target: CreateTemplateTarget,
+    #[clap(long, value_name = "KERNEL_VERSION")]
+    kernels: Option<String>,
+    #[clap(long, value_name = "PATH")]
+    destination: Option<String>,
+}
+
+#[derive(clap::ValueEnum, Copy, Clone, Debug, PartialEq, Eq)]
+enum CreateTemplateTarget {
+    V5,
+    Cortex,
+}
+
+#[derive(clap::Subcommand)]
+#[command(infer_subcommands = true)]
 enum Command {
-    Example,
+    Conductor {
+        #[clap(subcommand)]
+        command: ConductorSubcommand,
+    },
+
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = TopLevelArgs::parse();
 
     match args.command {
-        Command::Example => {
-            println!("Hello, world!");
-        }
+        Command::Conductor{command} => match command {
+            ConductorSubcommand::CreateTemplate(template_args) => {
+                println!("{:?}", template_args);
+            }
+        },
     }
 }
